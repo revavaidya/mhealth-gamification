c580920102d7e30419b72f34b5f5fbdfe2632c8f
Reva <iam.reva.vaidya@gmail.com>
2023-11-14 22:01:28 -0600

    added subdir to create all reviews

--- reviews.py
+++ reviews.py
@@ -2,14 +2,46 @@ from app_store_scraper import AppStore
 import pandas as pd
 import numpy as np
 import json
-strava = AppStore(country="us", app_name="strava")
-strava.review(how_many=1500)
+import os
+apps_list = ["Impulse - Brain Training",
+"Planet Fitness Workouts",
+"Flo Period Pregnancy Tracker",
+"MyFitness Pal Calorie Counter",
+"Yuka Food Cosmetic Scanner"
+]
 
-df = pd.DataFrame(np.array(strava.reviews),columns=['review'])
-df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
-df2.head()
+folder_path = os.getcwd()
+subdir_name = 'reviews'
+new_subdirectory_path = os.path.join(folder_path, subdir_name)
+if not os.path.exists(new_subdirectory_path):
+    # Create the new subdirectory
+        os.makedirs(new_subdirectory_path)
+        print(f"Subdirectory '{subdir_name}' created successfully.")
+created = False
 
-df2.to_csv('reviews_strava.csv')
+for x in apps_list:
+    app_idx = AppStore(country="us", app_name=x)
+    app_idx.review(how_many=100)
+
+    df = pd.DataFrame(np.array(app_idx.reviews),columns=['review'])
+    df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
+    df2['App_Name'] = x
+    df2.head()
+    store_csv = x + ".csv"
+    csv_file_path = os.path.join(new_subdirectory_path, store_csv)
+    df2.to_csv(csv_file_path, index=False)
+    print("Added", x, "to" , store_csv)
+    #created = True
+
+
+    #   strava = AppStore(country="us", app_name="Impulse - Brain Training")
+    # strava.review(how_many=1500)
+
+    # df = pd.DataFrame(np.array(strava.reviews),columns=['review'])
+    # df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
+    # df2.head()
+
+    # df2.to_csv('reviews_myimpusle.csv')
 
 
 # -*- coding: utf-8 -*-
