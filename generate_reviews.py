80dec5ab6b4fb1706dc987c4da2935e12941b77e
Reva <iam.reva.vaidya@gmail.com>
2023-11-16 11:48:36 -0600

    final version

--- generate_reviews.py
+++ generate_reviews.py
@@ -3,13 +3,42 @@ import pandas as pd
 import numpy as np
 import json
 import os
+
 apps_list = ["Impulse - Brain Training",
 "Planet Fitness Workouts",
 "Flo Period Pregnancy Tracker",
 "MyFitness Pal Calorie Counter",
-"Yuka Food Cosmetic Scanner"
+"Yuka Food Cosmetic Scanner",
+"ShutEye Sleep Tracker Sound",
+"Me Daily Routine Planner",
+"Blood Pressure App Health Body",
+"BetterMe Health Coaching",
+"JustFit Lazy Workout Fit",
+"Sweatcoin Walking Step Counter",
+"InPulse Heart Rate Monitor",
+"All Trails Hike Bike Run",
+"myCigna",
+"I am Daily Affirmations",
+"BetterSleep Relax and Sleep",
+"Paired Couples Relationship",
+"Finch Self Care Pet",
+"Motivation Daily quotes",
+"Aetna Health ",
+"Calm",
+"Lose It Calorie Counter",
+"Strava",
+"Peloton",
+"Nike Run Club",
+"Balance Meditation Sleep",
+"Mindbody Fitness Salon Spa",
+"Breeze Mental Health",
+"Calorie Counter My Net Diary",
+"Fitbit Health Fitness",
+"Peak"
 ]
 
+import time
+start = time.time()
 folder_path = os.getcwd()
 subdir_name = 'reviews'
 new_subdirectory_path = os.path.join(folder_path, subdir_name)
@@ -21,7 +50,7 @@ created = False
 
 for x in apps_list:
     app_idx = AppStore(country="us", app_name=x)
-    app_idx.review(how_many=100)
+    app_idx.review(how_many=1500)
 
     df = pd.DataFrame(np.array(app_idx.reviews),columns=['review'])
     df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
@@ -31,21 +60,8 @@ for x in apps_list:
     csv_file_path = os.path.join(new_subdirectory_path, store_csv)
     df2.to_csv(csv_file_path, index=False)
     print("Added", x, "to" , store_csv)
-    #created = True
+
+end = time.time()
+print("Time to compute using flair on leaderboard was")
 
 
-    #   strava = AppStore(country="us", app_name="Impulse - Brain Training")
-    # strava.review(how_many=1500)
-
-    # df = pd.DataFrame(np.array(strava.reviews),columns=['review'])
-    # df2 = df.join(pd.DataFrame(df.pop('review').tolist()))
-    # df2.head()
-
-    # df2.to_csv('reviews_myimpusle.csv')
-
-
-# -*- coding: utf-8 -*-
-# from bs4 import BeautifulSoup
-# import requestsheaders = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
-# url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=iphone &_sacat=0&LH_TitleDesc=0&Model=Apple%20iPhone%208&_sop=12&LH_PrefLoc=0&rt=nc&Storage%20Capacity=64%20GB&_dcat=9355'response=requests.get(url,headers=headers)
-# soup=BeautifulSoup(response.content,'lxml')
